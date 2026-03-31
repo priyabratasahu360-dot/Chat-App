a32a589e4df7d86431467876d1c52a54e02b0ce1
priyabratasahu-lab <priyabratasahu2005@gmail.com>
2026-03-31 12:01:15 +0530

    fixed profile page content

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -19,6 +19,7 @@ export const ProfilePage = () => {
         }
         console.log(authUser);
     }
+    const formatted = new Date(authUser.createdAt).toLocaleDateString("en-IN");
     return(
         <div className="h-screen pt-20">
             <div className="max-w-2xl mx-auto p-4 py-8">
@@ -70,7 +71,7 @@ export const ProfilePage = () => {
                         <div className="space-y-3 text-sm">
                             <div className="flex items-center justify-between py-2 border-b border-zinc-700">
                                 <span>Member Since</span>
-                                <span>{authUser.createdAt?.split("T")[0]}</span>
+                                <span>{formatted}</span>
                             </div>
                             <div className="flex items-center justify-between py-2">
                                 <span>Account Status</span>
